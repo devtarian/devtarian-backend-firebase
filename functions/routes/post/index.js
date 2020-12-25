4f420420024158d793b6f1989da9b8fe09f8bb0f
kyungyoonha <gkb10a@naver.com>
2020-12-25 23:15:07 +0900

    기능추가: GET /post 완성 - 위도 경도 쿼리로 데이터 조회

--- functions/routes/post/index.js
+++ functions/routes/post/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, fetchPost } = require("./post.ctrl");
+const { createPost, getPosts } = require("./post.ctrl");
 const { protect, filesUpload } = require("../../middleware");
 
-// router.get("/", fetchPost);
+router.get("/", getPosts);
 router.post("/", protect, filesUpload, createPost);
 
 module.exports = router;
--- functions/routes/post/post.ctrl.js
+++ functions/routes/post/post.ctrl.js
@@ -1,13 +1,18 @@
 const { firebase, admin, db } = require("../../fbAdmin");
 const config = require("../../config");
+const { GeoFirestore } = require("geofirestore");
 
 exports.createPost = async (req, res) => {
     try {
         let { lat, lng, ...newPost } = req.body;
-        newPost.location = new admin.firestore.GeoPoint(lat, lng);
+
+        const geoFirestore = new GeoFirestore(db);
+        const collection = geoFirestore.collection("posts");
+
+        newPost.coordinates = new admin.firestore.GeoPoint(lat, lng);
         newPost.createAt = new Date().toISOString();
 
-        await db.collection("posts").add(newPost);
+        await collection.add(newPost);
         return res.status(200).json(newPost);
     } catch (err) {
         console.log(err);
@@ -15,23 +20,28 @@ exports.createPost = async (req, res) => {
     }
 };
 
-// firestore geopoint javascript
-// firestore geopoint query "node"
-// firebase get data by lat lng
-// https://stackoverflow.com/questions/47092198/how-to-save-geopoint-in-firestore-using-query
-// https://levelup.gitconnected.com/nearby-location-queries-with-cloud-firestore-e7f4a2f18f9d
-// https://jsbin.com/firoteluju/2/edit?js,console
-// https://jsbin.com/zogeyar/edit?js,console
-
-exports.fetchPost = async (req, res) => {
+exports.getPosts = async (req, res) => {
     try {
-        console.log("------");
-        console.log(req.body);
-        console.log("------");
-        return res.ststus(200).json({ message: "success" });
+        const { lat, lng } = req.query;
+        const geofirestore = new GeoFirestore(db);
+        const geocollection = geofirestore.collection("posts");
+        const snapshot = await geocollection
+            .near({
+                center: new firebase.firestore.GeoPoint(
+                    Number(lat),
+                    Number(lng)
+                ),
+                radius: 10, // km
+            })
+            .get();
+
+        const posts = snapshot.docs.map((doc) => doc.data());
+
+        return res.status(200).json({ posts });
     } catch (err) {
+        console.log(err);
         return res.status(500).json({
-            error: "500 서버에러",
+            error: err,
         });
     }
 };
